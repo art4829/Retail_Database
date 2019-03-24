f61412dd0c8c036bf67ab390673d9fc62f886639
art4829 <abhaya.tmkr@gmail.com>
2019-03-24 01:20:40 -0400

    Orders

---
+++ src/Database/Populate/OrderTable.java
@@ -0,0 +1,101 @@
+package Database.Populate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class OrderTable {
+    public static void createOrderTable(Connection conn){
+        try {
+            String query = "Create table orders(\n" +
+                    "order_id varchar(255) primary key,\n" +
+                    "num varchar(255),\n" +
+                    "street varchar(255),\n" +
+                    "city varchar(255),\n" +
+                    "state varchar(255),\n" +
+                    "zip varchar(255),\n" +
+                    "customer_id varchar(255));" ;
+
+            /**
+             * Create a query and execute
+             */
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void populateOrderTable(Connection conn,
+                                           String fileName)
+            throws SQLException {
+        /**
+         * Structure to store the data as you read it in
+         * Will be used later to populate the table
+         *
+         * You can do the reading and adding to the table in one
+         * step, I just broke it up for example reasons
+         */
+        ArrayList<Order> people = new ArrayList<Order>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                people.add(new Order(split));
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        String sql = createOrderInsertSQL(people);
+
+        /**
+         * Create and execute an SQL statement
+         *
+         * execute only returns if it was successful
+         */
+        Statement stmt = conn.createStatement();
+        stmt.execute(sql);
+    }
+
+    public static String createOrderInsertSQL(ArrayList<Order> customers){
+        StringBuilder sb = new StringBuilder();
+
+        /**
+         * The start of the statement,
+         * tells it the table to add it to
+         * the order of the data in reference
+         * to the columns to ad dit to
+         */
+        sb.append("INSERT INTO orders (order_id, num, street, city, state, zip, customer_id) VALUES");
+
+        /**
+         * For each person append a (id, first_name, last_name, MI) tuple
+         *
+         * If it is not the last person add a comma to seperate
+         *
+         * If it is the last person add a semi-colon to end the statement
+         */
+        for(int i = 0; i < customers.size(); i++){
+            Order c = customers.get(i);
+            sb.append(String.format("(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')",
+                    c.getOrder_id(), c.getNum(), c.getStreet(), c.getCity(), c.getState(), c.getZip(), c.getCustomer_id()));
+            if( i != customers.size()-1){
+                sb.append(",");
+            }
+            else{
+                sb.append(";");
+            }
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+}
