730d83dbac95d7ee183cf11eb11f5e76f807c289
James Oh <jo9347@rit.edu>
2019-03-24 11:07:40 -0400

    Added reorder_id in order.csv and start developing vendor application

--- src/Applications/VenderApplication.java
+++ src/Applications/VenderApplication.java
@@ -1,5 +1,33 @@
 package Applications;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class VenderApplication {
+    private Connection connection;
+    private final String EMAIL_DOESNT_EXIST="Email Address Doesn't exist\nPlease Sign-up!";
+    private final String INCORRECT_PW="Incorrect password, Please enter again";
+    public void createConnection(String location,
+                                 String user,
+                                 String password){
+        try {
+
+            //This needs to be on the front of your location
+            String url = "jdbc:h2:" + location;
+
+            //This tells it to use the h2 driver
+            Class.forName("org.h2.Driver");
+
+            //creates the connection
+            connection = DriverManager.getConnection(url,
+                    user,
+                    password);
+        } catch (SQLException | ClassNotFoundException e) {
+            //You should handle this better
+            e.printStackTrace();
+        }
+    }
+
 
 }
