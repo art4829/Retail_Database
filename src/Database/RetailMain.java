d7487c3ff74a7de1e32593455866fd2b75990376
art4829 <abhaya.tmkr@gmail.com>
2019-03-24 23:26:13 -0400

    Updated Product

--- src/Database/RetailMain.java
+++ src/Database/RetailMain.java
@@ -72,10 +72,10 @@ public class RetailMain {
              * Creates a sample Person table
              * and populates it from a csv file
              */
-            PantryTable.createTable(rm.getConnection());
-            PantryTable.populateTable(
+            ProductsTable.createTable(rm.getConnection());
+            ProductsTable.populateTable(
                     rm.getConnection(),
-                    "C:\\Users\\abhay\\IdeaProjects\\Retail_Database\\src\\Datasets\\Pantry\\pantry.csv");
+                    "C:\\Users\\abhay\\IdeaProjects\\Retail_Database\\src\\Datasets\\products.csv");
 
             /**
              * Just displays the table
