d2ef12718bd71600b476107c8146abe8275a08e9
art4829 <abhaya.tmkr@gmail.com>
2019-04-03 22:52:05 -0400

    Able to buy products. Need to update database though

--- src/Applications/OnlineApplication.java
+++ src/Applications/OnlineApplication.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.CheckedOutputStream;
 
 public class OnlineApplication {
 
@@ -185,6 +186,60 @@ public class OnlineApplication {
         return arry;
     }
 
+    public String getUPC(String name, Connection connection){
+        String retString="";
+        try {
+            String query = "SELECT UPC FROM  Product where name='" + name + "'";
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet r = stmt.executeQuery(query);
+            r.next();
+            retString=r.getString(1);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return retString;
+    }
+
+    public String getPrice(String UPC){
+        String price="";
+        try {
+            String query = "Select price from product \n" +
+                    "where UPC = '"+UPC+"';";
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet r = stmt.executeQuery(query);
+            r.next();
+            price =r.getString(1);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return price;
+    }
+
+    public void buyProduct(String UPC){
+        String amount="";
+        int updateAmt=0;
+        try {
+            String query = "Select amount from contains \n" +
+                    "where UPC = '"+UPC+"' and store_id = '5';";
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet r = stmt.executeQuery(query);
+            r.next();
+            amount=r.getString(1);
+            updateAmt=Integer.parseInt(amount)-1;
+            String updatequery="Update contains\n" +
+                    "set amount='"+updateAmt+"' where upc='"+UPC+"' and store_id='5';";
+            Statement stm2= connection.createStatement();
+            stm2.execute(updatequery);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * Run the application
      * @param args
@@ -224,146 +279,158 @@ public class OnlineApplication {
         }
 
         System.out.println("Welcome To Our Online Store! \n\n");
-        System.out.println("What Product will you like to Buy today?\n");
-        System.out.println("Press 1 for Bakery");
-        System.out.println("Press 2 for Beverage");
-        System.out.println("Press 3 for Grocery");
-        System.out.println("Press 4 for Pantry\n\n");
+        while(true) {
+            System.out.println("What Product will you like to Buy today?\n");
+            System.out.println("Press 1 for Bakery");
+            System.out.println("Press 2 for Beverage");
+            System.out.println("Press 3 for Grocery");
+            System.out.println("Press 4 for Pantry\n\n");
 
-        int product_choice = scan.nextInt();
-        int count;
-        ArrayList<String> itms;
-        int answer = 0;
-        Map<Integer, String> menu= new HashMap<Integer, String>() {
-        };
-        switch (product_choice) {
-            case 1:
-                System.out.println("Press 1 for Bakery");
-                System.out.println("Press 2 for Bread");
-                System.out.println("Press 3 for Pastry\n\n");
-                int bakery_choice = scan.nextInt();
+            int product_choice = scan.nextInt();
+            int count;
+            ArrayList<String> itms;
+            int answer = 0;
+            Map<Integer, String> menu= new HashMap<Integer, String>();
 
-                switch (bakery_choice) {
-                    case 1:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "bakery");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                    case 2:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "bread");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                    case 3:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "pastry");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                }
-                break;
-            case 2:
-                count = 1;
-                itms = app.viewItem(app.getConnection(), "beverage");
-                System.out.println("Please enter the number of the Item:- ");
-                for (String it : itms) {
-                    System.out.println(count + ": " + it);
-                    menu.put(count,it);
-                    count += 1;
-                }
-                answer=scan.nextInt();
-                break;
-            case 3:
-                System.out.println("Press 1 for Dairy");
-                System.out.println("Press 2 for Grocery");
-                System.out.println("Press 3 for Meat");
-                System.out.println("Press 3 for Produce\n\n");
-                int grocery_choice = scan.nextInt();
+            switch (product_choice) {
+                case 1:
+                    System.out.println("Press 1 for Bakery");
+                    System.out.println("Press 2 for Bread");
+                    System.out.println("Press 3 for Pastry\n\n");
+                    int bakery_choice = scan.nextInt();
 
-                switch (grocery_choice) {
-                    case 1:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "dairy");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                    case 2:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "grocery");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                    case 3:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "meat");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                    case 4:
-                        count = 1;
-                        itms = app.viewItem(app.getConnection(), "produce");
-                        System.out.println("Please enter the number of the Item:- ");
-                        for (String it : itms) {
-                            System.out.println(count + ": " + it);
-                            menu.put(count,it);
-                            count += 1;
-                        }
-                        answer=scan.nextInt();
-                        break;
-                }
+                    switch (bakery_choice) {
+                        case 1:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "bakery");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                        case 2:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "bread");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                        case 3:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "pastry");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                    }
+                    break;
+                case 2:
+                    count = 1;
+                    itms = app.viewItem(app.getConnection(), "beverage");
+                    System.out.println("Please enter the number of the Item:- ");
+                    for (String it : itms) {
+                        System.out.println(count + ": " + it);
+                        menu.put(count, it);
+                        count += 1;
+                    }
+                    answer = scan.nextInt();
+                    break;
+                case 3:
+                    System.out.println("Press 1 for Dairy");
+                    System.out.println("Press 2 for Grocery");
+                    System.out.println("Press 3 for Meat");
+                    System.out.println("Press 4 for Produce\n\n");
+                    int grocery_choice = scan.nextInt();
+
+                    switch (grocery_choice) {
+                        case 1:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "dairy");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                        case 2:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "grocery");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                        case 3:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "meat");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                        case 4:
+                            count = 1;
+                            itms = app.viewItem(app.getConnection(), "produce");
+                            System.out.println("Please enter the number of the Item:- ");
+                            for (String it : itms) {
+                                System.out.println(count + ": " + it);
+                                menu.put(count, it);
+                                count += 1;
+                            }
+                            answer = scan.nextInt();
+                            break;
+                    }
+                    break;
+                case 4:
+                    count = 1;
+                    itms = app.viewItem(app.getConnection(), "pastry");
+                    System.out.println("Please enter the number of the Item:- ");
+                    for (String it : itms) {
+                        System.out.println(count + ": " + it);
+                        menu.put(count, it);
+                        count += 1;
+                    }
+                    answer = scan.nextInt();
+                    break;
+
+            }
+            System.out.println("\n You have chosen: " + menu.get(answer));
+            String UPC = app.getUPC(menu.get(answer), app.getConnection());
+            System.out.println("The price for the chosen item is: " + app.getPrice(UPC));
+            System.out.println("Do you still want to buy it?(Y/N)");
+            Scanner buyoption = new Scanner(System.in);
+            String option = buyoption.nextLine().toLowerCase();
+
+
+            if(option.equals("y")){
+                    app.buyProduct(UPC);
+                    System.out.println("Congratulations, you have succesfully bought "+menu.get(answer));
+            }
+            System.out.println("Would you like to look at another item?(Y/N)");
+            String option2 = buyoption.nextLine().toLowerCase();
+            if(option2.equals("n")){
                 break;
-            case 4:
-                count = 1;
-                itms = app.viewItem(app.getConnection(), "pastry");
-                System.out.println("Please enter the number of the Item:- ");
-                for (String it : itms) {
-                    System.out.println(count + ": " + it);
-                    menu.put(count,it);
-                    count += 1;
-                }
-                answer=scan.nextInt();
-                break;
-                
+            }
         }
-        System.out.println("\n You have chosen: "+ menu.get(answer));
-
-
-
-
-
-
 
     }
 }
