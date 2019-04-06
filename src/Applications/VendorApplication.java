c33fade98842f513e0dbf9ef7162925d451e88a3
James Oh <jo9347@rit.edu>
2019-04-06 16:11:53 -0400

    Vendor login started implementing

--- src/Applications/VendorApplication.java
+++ src/Applications/VendorApplication.java
@@ -1,8 +1,7 @@
 package Applications;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 public class VendorApplication {
     private Connection connection;
@@ -29,5 +28,39 @@ public class VendorApplication {
         }
     }
 
+    public String loginVendor(Connection connection, String vendor_id){
+        String query="select vendor_name, password from vendor_id where email = "+"\'"+vendor_id+"\'\n";
+        String vendor_name;
+        String password;
 
+        try {
+            /**
+             * Execute the query and return the result set
+             */
+            Statement stmt = connection.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+            result.next();
+
+            vendor_name=result.getString("vendor_name");
+            password=result.getString("password");
+
+
+            Scanner pw= new Scanner(System.in);
+            System.out.print("Please Enter your Password: ");
+            String password_input =pw.nextLine();
+
+            // if incorrect password
+            while(!password_input.equals(password)){
+                System.out.println(INCORRECT_PW);
+                System.out.print("Please Enter your Password: ");
+                password_input = pw.nextLine();
+            }
+            return "Welcome! " +vendor_name;
+
+
+        } catch (SQLException e) {
+            return EMAIL_DOESNT_EXIST;
+        }
+
+    }
 }
