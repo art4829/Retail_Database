8b3bffa40c8151cecdf4d622f2b84e2526855017
James Oh <jo9347@rit.edu>
2019-04-06 16:42:14 -0400

    Vendor application

--- src/Applications/VendorApplication.java
+++ src/Applications/VendorApplication.java
@@ -2,11 +2,14 @@ package Applications;
 
 import java.sql.*;
 import java.util.Scanner;
+import java.lang.String;
+import java.sql.*;
 
 public class VendorApplication {
     private Connection connection;
     private final String EMAIL_DOESNT_EXIST="Email Address Doesn't exist\nPlease Sign-up!";
     private final String INCORRECT_PW="Incorrect password, Please enter again";
+
     public void createConnection(String location,
                                  String user,
                                  String password){
@@ -62,5 +65,65 @@ public class VendorApplication {
             return EMAIL_DOESNT_EXIST;
         }
 
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public void executeQuery(Statement stmt, String query) throws SQLException {
+        //check if it is select
+        Boolean ret = stmt.execute(query);
+        if (ret) {
+            ResultSet result = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = result.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            // The column count starts from 1
+
+            //ArrayList<String> column_names = new ArrayList<String>();
+            for (int i = 1; i <= columnCount; i++) {
+                String name = rsmd.getColumnName(i);
+                //column_names.add(name);
+                System.out.print(name + " ");
+                // Do stuff with name
+            }
+            System.out.println();
+
+            while (result.next()) {
+                for (int i = 0; i < columnCount; i++) {
+                    System.out.print(result.getString(i + 1) + " ");
+                }
+                System.out.println();
+            }
+            // STEP 5: Clean-up environment
+            result.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        VendorApplication app = new VendorApplication();
+        // Hard drive location of database
+        String location = "./retailDb/retailDb";
+        String user = "cskid";
+        String password = "retaildomain";
+        //Create database connection
+        app.createConnection(location, user, password);
+
+        Connection conn = app.getConnection();
+
+        //create view
+        try {
+            Statement stmt = conn.createStatement();
+
+            String query = "create view v as select * from Reorder;";
+            stmt.execute(query);
+
+            //test: print the view
+            query = "select * from view";
+            app.executeQuery(stmt,query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
