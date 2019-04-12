a3cc129d75ae02d0efe7b7a127e5ea0b83696465
Straviaz <jxs2143@rit.edu>
2019-04-12 09:48:56 -0400

    Added sign in as guest option

--- src/Applications/CheckRegisterApplication.java
+++ src/Applications/CheckRegisterApplication.java
@@ -7,6 +7,30 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class CheckRegisterApplication extends MethodCalls {
+    private Connection connection;
+
+    @Override
+    public void buyProduct(String UPC) {
+        String amount = "";
+        int updateAmt = 0;
+        try {
+            String query = "Select amount from contains \n" +
+                    "where UPC = '" + UPC + "' and store_id = '5';";
+            Statement stmt = connection.createStatement();
+
+            ResultSet r = stmt.executeQuery(query);
+            r.next();
+            amount = r.getString(1);
+            updateAmt = Integer.parseInt(amount) - 1;
+            String updatequery = "Update contains\n" +
+                    "set amount='" + updateAmt + "' where upc='" + UPC + "' and store_id='5';";
+
+            Statement stm2 = connection.createStatement();
+            stm2.execute(updatequery);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main( String[] args ) {
         MethodCalls app = new CheckRegisterApplication();
@@ -19,11 +43,12 @@ public class CheckRegisterApplication extends MethodCalls {
         app.createConnection(location, user, password);
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("|------------------------------------------|");
-        System.out.println("|-------------WELCOME TO RETAIL------------|");
-        System.out.println("|----------Please enter 1 to LOGIN---------|");
-        System.out.println("|----------Please enter 2 to SIGNUP--------|");
-        System.out.println("|------------------------------------------|");
+        System.out.println("|-----------------------------------------------------|");
+        System.out.println("|-------------WELCOME TO RETAIL-----------------------|");
+        System.out.println("|----------Please enter 1 to LOGIN--------------------|");
+        System.out.println("|----------Please enter 2 to SIGNUP-------------------|");
+        System.out.println("|----------Please enter 3 to CHECKOUT AS GUEST--------|");
+        System.out.println("|-----------------------------------------------------|");
         int check = scan.nextInt();
 
         if (check == 1) {
@@ -39,37 +64,64 @@ public class CheckRegisterApplication extends MethodCalls {
             // If sign up, Register user
             System.out.println("Registering you");
             app.register(app);
+        } else if(check == 3) {
+            //No additional information required
+            System.out.println("Checking out as guest");
         }
+
+
         try {
             TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("|--------------------------------------|");
+        System.out.println("\n\n|--------------------------------------|");
         System.out.println("|     Welcome To Our Online Store!     |");
         System.out.println("| To SignOut please press 0 at anytime |");
         System.out.println("|--------------------------------------|\n\n");
 
         while(true) {
-            int answer = 0;
 
-            System.out.println("|---------------------|");
-            System.out.println("| Press 1 to Checkout |");
-            System.out.println("|---------------------|\n\n");
-            answer = scan.nextInt();
+            System.out.println("|-----------------------|");
+            System.out.println("| Press 1 to Checkout   |");
+            System.out.println("|-----------------------|");
+            int answer = scan.nextInt();
+            int storeID = 0;
 
             if( answer == 1 ) {
+                while(true) {
+                    System.out.println("Please enter the store ID: ");
+                    storeID = scan.nextInt();
+
+                    //Checkif valid store ID
+                    if( storeID > 0 && storeID < 5 ) {
+                        break;
+                    }
+                    System.out.println("Invalid store ID.");
+                }
+
                 System.out.println("Please enter the product UPC: ");
                 String UPC = "";
                 UPC = scan.next();
 
                 //reduce the number of item in the database
-                app.buyProduct(UPC);
+                System.out.println("\n\n");
+                System.out.println(">>>>>>>----- The price for the chosen item is: " + app.getPrice(UPC));
+                System.out.println(">>>>>>>----- Do you still want to buy it?(Y/N)");
+
+                String option = "";
+                option = scan.next();
+
+                //check if the customer still wants to buy the item
+                if( option.toLowerCase().equals("y") || option.toLowerCase().equals("yes") ) {
+                    app.buyProductStore(UPC,storeID);
+                    System.out.println(">>>>>>>----- Congratulations, you have successfully bought the item\n\n");
+                }
+
             } else if( answer == 0 ) {
                 break;
             }
-
         }
     }
 }
