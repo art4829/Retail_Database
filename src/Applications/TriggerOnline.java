6ddc04cf47f022f5c36fb0b4b8a5eacd3d955e3f
jack8558 <jo8842@g.rit.edu>
2019-04-14 20:20:44 -0400

    comment back

--- src/Applications/TriggerOnline.java
+++ src/Applications/TriggerOnline.java
@@ -68,7 +68,20 @@ public class TriggerOnline extends MethodCalls {
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:./retailDb/retailDb", "cskid", "retaildomain");
         Statement stat = conn.createStatement();
-        
+
+//        stat.execute("Drop trigger updateOnline");
+//        stat.execute("CREATE TRIGGER updateOnline " +
+//                "AFTER UPDATE ON CONTAINS FOR EACH ROW " +
+//               "CALL \"Applications.TriggerOnline$MyTrigger\" ");
+//
+
+//        stat.execute("INSERT INTO INVOICE VALUES(1, 10.0)");
+//        stat.execute("INSERT INTO INVOICE VALUES(2, 19.95)");
+//        stat.execute("UPDATE INVOICE SET AMOUNT=20.0 WHERE ID=2");
+//        stat.execute("DELETE FROM INVOICE WHERE ID=1");
+//        stat.execute("Insert into contains values('2','915000809965','11')");
+//        stat.execute("Update contains set amount = '9' where UPC='413874160000' and store_id = '1'");
+
         ResultSet rs;
         rs = stat.executeQuery("SELECT * FROM reorder");
 
